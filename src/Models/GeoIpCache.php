7a80d35dfa74dc9672d0e15fec6a8fcf0f858fc6
Ozan Kurt <kurtozan@gmail.com>
2026-04-10 22:01:08 +0200

    fix(tracker): add PHPDoc properties to GeoIpCache model for static analysis

--- src/Models/GeoIpCache.php
+++ src/Models/GeoIpCache.php
@@ -5,7 +5,20 @@ declare(strict_types=1);
 namespace OzanKurt\Tracker\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property int                 $id
+ * @property string              $ip_hash
+ * @property string|null         $country_code
+ * @property string|null         $country_name
+ * @property string|null         $city
+ * @property float|null          $latitude
+ * @property float|null          $longitude
+ * @property string              $provider
+ * @property Carbon              $cached_until
+ * @property Carbon              $created_at
+ */
 class GeoIpCache extends Model
 {
     public $timestamps = false;
